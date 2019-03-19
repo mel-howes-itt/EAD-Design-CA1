d464c617dbb4c8c4ac136dc9b8b6e5baa25f2d2b
Mel Howes <melanie.howes@gmail.com>
2019-03-19 18:35:48 +0000

    Added ascii image.

--- tud_cm.py
+++ tud_cm.py
@@ -12,6 +12,18 @@ mydisruptedids = []
 x = 0
 
 print('\n' * 5)
+print(r"""See no evil,    hear no evil,    speak no evil,    deliver chaos!
+
+       .---.            .---.            .---.           .---.
+     _/_-.-_\_        _/.-.-.\_        _/.-.-.\_       _/.-.-.\_
+    / __} {__ \      /|( o o )|\      ( ( o o ) )     ( ( o o ) )
+   / //     \\ \    | //     \\ |      |/     \|       |/     \|
+  / / \ --- / \ \  / / \ --- / \ \      \ /^\ /         \ .-. /
+  \ \_/-----\_/ /  \ \_/-----\_/ /      / \ / \         /-----\
+   \           /    \           /      /  /|\  \       /       \
+
+""")
+print()
 print ('Welcome to Mels TUD Chaos Monkey (tud_cm)')
 print ('----------------------------------------')
 print()
